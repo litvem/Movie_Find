030c94ef0d4d01712fb15a57d4a342eca4fcf015
Emma Litvin <guslitvem@student.gu.se>
2024-08-21 19:38:51 +0200

    Add MovieCard element and use it to dynamically render movies from DB on the web page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,60 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 
-// MAin functional component
+import './App.css';
+import SearchIcon from './search.svg';
+import MovieCard from './MovieCard';
+
+// API URL to movie DB
+const API_URL = 'http://www.omdbapi.com?apikey=ba604c3a';
+
+// Main functional component
 const App = () => {
+    const [movies, setMovies] = useState([]);
+
+    // Asynchronous function that requires time to load the movies that will search for movies based om given title
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        // Get the data from the response
+        const data = await response.json();
+
+        setMovies(data.Search);
+    }
+
+    // Load data from DB as the page loads
+    useEffect(() => {
+        searchMovies('Spiderman');
+    }, []);
+
     return (
-        <h1>App</h1>
+        <div className='app'>
+            <h1>MovieFind</h1>
+
+            <div className='searchBar'>
+                <input 
+                    placeholder="Search for movies"
+                    value="Spiderman"
+                    onChange={() => {}}
+                />
+                <img 
+                    src={SearchIcon}
+                    alt="search"
+                    onClick={() => {}}
+                />
+            </div>
+
+            {movies?.length > 0
+                ? (
+                    <div className='container'>
+                        {movies.map((movie) => (
+                            <MovieCard movie={movie} />
+                        ))}
+                    </div>
+                ) : (
+                    <div className='empty'>
+                        <h2>No movies found</h2>
+                    </div>
+                )}
+        </div>
     );
 }
 
